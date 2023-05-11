920c10587af6744fb66cb891fcad511598820a97
preksha <jainpreksha2712@gmail.com>
2023-05-11 22:34:44 +0530

    added tweet service with hashtag processing

--- src/service/tweet-service.js
+++ src/service/tweet-service.js
@@ -1,8 +1,9 @@
-const { TweetRepository } = require('../repository/index') 
+const { TweetRepository, HashtagRepository } = require('../repository/index') 
 
 class TweetService {
     constructor(){
-        this.tweetRepository = new TweetRepository()
+        this.tweetRepository = new TweetRepository();
+        this.hashtagRepository = new HashtagRepository();
     }
 
     async create(data){
@@ -12,6 +13,17 @@ class TweetService {
             let hashtags = content.hashtag(hashtagRegex);
             hashtags = hashtags.map(tag => tag.substring().toLowerCase());
             const tweet = await this.tweetRepository.create(data);
+            let alreadyPresentTags = await this.hashtagRepository.findByName(hashtags);
+            let titleOfTags = alreadyPresentTags.map(tag => tag.title);
+            let newTags = hashtags.filter(tag => !titleOfTags.includes(tag));
+            newTags = newTags.map(tag => {
+                return {title: tag, tweets: [tweet.id]};
+            });
+            const response = await this.hashtagRepository.bulkCreate(newTags);
+            alreadyPresentTags.forEach(tag => {
+                tag.tweets.push(tweet.id);
+                tag.save();
+            })
             return tweet;
         }catch(error){
             console.log('Service layer error');
