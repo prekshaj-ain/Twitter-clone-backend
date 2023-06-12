027f9aa0e4d07706d87f3abbddcc7aca0b1307ed
preksha <jainpreksha2712@gmail.com>
2023-06-12 21:51:03 +0530

    added more fields in user model

--- src/models/user.js
+++ src/models/user.js
@@ -1,28 +1,47 @@
 const { default: mongoose } = require("mongoose");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     email: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     name: {
-        type: String,
-        required: true,
-    }
-},{timestamps: true});
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+    },
+    coverPicture: {
+      type: String,
+    },
+    bio: {
+      type: String,
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
 
-userSchema.pre('save',function(next){
-    const encryptedpassword = bcrypt.hashSync(this.password,12);
-    this.password = encryptedpassword;
-    next();
-})
+userSchema.pre("save", function (next) {
+  const encryptedpassword = bcrypt.hashSync(this.password, 12);
+  this.password = encryptedpassword;
+  next();
+});
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
